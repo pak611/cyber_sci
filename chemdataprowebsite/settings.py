3dfe1d678b42449834e2e1c1759656313089a9b0
Patrick Kampmeyer <pak611@utulsa.edu>
2022-10-20 15:57:04 -0500

    change .env

--- chemdataprowebsite/settings.py
+++ chemdataprowebsite/settings.py
@@ -113,20 +113,20 @@ WSGI_APPLICATION = "chemdataprowebsite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": os.environ.get('DB_NAME'),
-#        "USER": os.environ.get('DB_USER'),
-#        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
-#        "HOST": os.environ.get('DB_HOST'),
-#        "PORT": 5432
-#    }
-#}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_USER_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": 5432
+    }
+}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+#db_from_env = dj_database_url.config(conn_max_age=600)
 #DATABASES['default'].update(db_from_env)
 
 # Password validation
