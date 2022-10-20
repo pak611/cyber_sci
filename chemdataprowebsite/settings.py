6408cc88706b61028fdacf724b8f5b750e593fa9
Patrick Kampmeyer <pak611@utulsa.edu>
2022-10-20 15:18:38 -0500

    change database settings file

--- chemdataprowebsite/settings.py
+++ chemdataprowebsite/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 #DATABASES['default'].update(db_from_env)
